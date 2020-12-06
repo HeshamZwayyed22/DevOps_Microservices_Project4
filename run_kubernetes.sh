7c88d449b9577687539ff85379a032275d115f59
root <root@ip-172-31-35-184.us-west-2.compute.internal>
2020-12-06 14:21:36 +0000

    Tasks 4, 5 & 6

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,15 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=zwayyed00/myapp
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deployment my-app --image=$dockerpath:first-image --port=80
 
 # Step 3:
 # List kubernetes pods
-
+kubectl get po
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward deployment/my-app 8000:80
--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,13 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-
+dockerpath=zwayyed00/myapp
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login --username=zwayyed00 
+docker tag myapp $dockerpath:first-image
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
